5def8c1fe5c39583b77ecb5a452f296ed93e8b53
Harry Cheng <chengyuhui1@gmail.com>
2020-11-07 16:14:36 +0800

    Move flag to the right place

--- src/hash.rs
+++ src/hash.rs
@@ -236,7 +236,20 @@ impl<Kind: AlgorithmKind> HashAlgorithm<Kind> {
         })
     }
 
-    fn create_hash(&self, secret: Option<&[u8]>, iv: Option<&[u8]>, hmac: bool) -> Result<Hash> {
+    /// Open a hash algorithm provider in HMAC mode
+    /// Do not use with [`MacAlgorithmId`]
+    /// 
+    /// [`MacAlgorithmId`]: ./enum.MacAlgorithmId.html
+    pub fn open_hmac(id: Kind) -> Result<Self> {
+        let handle = AlgoHandle::open_flags(id.to_str(), BCRYPT_ALG_HANDLE_HMAC_FLAG)?;
+
+        Ok(Self {
+            handle,
+            _kind: PhantomData,
+        })
+    }
+
+    fn create_hash(&self, secret: Option<&[u8]>, iv: Option<&[u8]>) -> Result<Hash> {
         let (sec_ptr, sec_len) = secret
             .map(|x| (x.as_ptr(), x.len()))
             .unwrap_or((std::ptr::null(), 0));
@@ -252,7 +265,7 @@ impl<Kind: AlgorithmKind> HashAlgorithm<Kind> {
                 object.len() as ULONG,
                 sec_ptr as *mut _,
                 sec_len as ULONG,
-                if hmac { BCRYPT_ALG_HANDLE_HMAC_FLAG } else { 0 },
+                0,
             ))?;
         };
 
@@ -270,13 +283,13 @@ impl<Kind: AlgorithmKind> HashAlgorithm<Kind> {
 impl HashAlgorithm<HashAlgorithmId> {
     /// Creates a new hash from the algorithm
     pub fn new_hash(&self) -> Result<Hash> {
-        self.create_hash(None, None, false)
+        self.create_hash(None, None)
     }
 
     /// Creates a new Message Authentication Code (MAC), if supported by the
     /// backing algorithm.
     pub fn new_mac(&self, secret: &[u8], iv: Option<&[u8]>) -> Result<Hash> {
-        self.create_hash(Some(secret), iv, true)
+        self.create_hash(Some(secret), iv)
     }
 }
 
@@ -286,7 +299,7 @@ impl HashAlgorithm<MacAlgorithmId> {
     ///
     /// Passing IV is required for GMAC mode, otherwise don't pass it for OMAC.
     pub fn new_mac(&self, secret: &[u8], iv: Option<&[u8]>) -> Result<Hash> {
-        self.create_hash(Some(secret), iv, true)
+        self.create_hash(Some(secret), iv)
     }
 }
 
